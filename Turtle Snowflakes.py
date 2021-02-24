0511c8c520eef07439f7e75825785e835c897a06
christina.lanaski <christina.lanaski@outlook.com>
2021-02-23 17:44:25 -0800

    Completed Turtle Snowflakes Python Project

--- Turtle Snowflakes.py
+++ Turtle Snowflakes.py
@@ -1,11 +1,24 @@
+import turtle
 import random
 pat = turtle.Turtle()
-turtle.Screen().bgcolor("blue")
-pat.color("yellow")
-for i in range(10):
-    for i in range(2):
-        pat.forward(100)
-        pat.right(60)
-        pat.forward(100)
-        pat.right(120)
-    pat.right(35)
+turtle.Screen().bgcolor("grey")
+colors = ["cyan", "purple", "white", "blue"]
+pat.penup()
+pat.forward(90)
+pat.left(45)
+pat.pendown()
+def branch():
+    for i in range(3):
+        for i in range (3):
+            pat.forward(30)
+            pat.backward(30)
+            pat.right(45)
+        pat.left(90)
+        pat.backward(30)
+        pat.left(45)
+    pat.right(90)
+    pat.forward(90)
+for i in range(8):
+    branch()
+    pat.left(45)
+#    pat.color(random.choice(colors))
